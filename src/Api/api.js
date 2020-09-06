7801097767a242b893e245b8e55576103901b9a7
Seongkyun-Yu <ysungkyun@gmail.com>
2020-09-06 23:11:30 +0900

    feat: Make getMarketCodes, getInitCandles api

---
+++ src/Api/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const coinApi = {
+  getMarketCodes: () =>
+    axios.get("https://api.upbit.com/v1/market/all?isDetails=false"),
+  getInitCanldes: (coins) =>
+    axios.get(`https://api.upbit.com/v1/ticker?markets=${coins}`),
+};
