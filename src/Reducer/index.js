5ae4439a207303d532c83c18e4fafe78dad90c34
Seongkyun-Yu <ysungkyun@gmail.com>
2020-09-07 03:49:19 +0900

    feat: Make rootSaga

--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -1,10 +1,15 @@
 import { combineReducers } from "redux";
-import { candleReducer } from "./candleReducer";
+import { candleReducer, candleSaga } from "./candleReducer";
 import { loadingReducer } from "./loadingReducer";
+import { all } from "redux-saga/effects";
 
 const rootReducer = combineReducers({
   Candle: candleReducer,
   Loading: loadingReducer,
 });
 
-export { rootReducer };
+function* rootSaga() {
+  yield all([candleSaga()]);
+}
+
+export { rootReducer, rootSaga };
