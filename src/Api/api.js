61661e7033356f462aab4f6dc260d48089b26abd
Seongkyun-Yu <ysungkyun@gmail.com>
2020-09-12 01:47:23 +0900

    fix: fix getOneCoinCandles api 데이터 오는 순서가 시간 내림차순으로 와서 시간 오름차순으로 오게끔 바꿈

--- src/Api/api.js
+++ src/Api/api.js
@@ -6,7 +6,14 @@ export const coinApi = {
   getInitCanldes: (coins) =>
     axios.get(`https://api.upbit.com/v1/ticker?markets=${coins}`),
   getOneCoinCandles: ({ coin, timeType, timeCount }) =>
-    axios.get(
-      `https://api.upbit.com/v1/candles/${timeType}/${timeCount}?market=${coin}&count=200`
-    ),
+    axios
+      .get(
+        `https://api.upbit.com/v1/candles/${timeType}/${timeCount}?market=${coin}&count=200`
+      )
+      .then((res) => {
+        return {
+          ...res,
+          data: res.data.sort((a, b) => a.timestamp - b.timestamp),
+        };
+      }),
 };
