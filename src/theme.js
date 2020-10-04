3a1d579fd43083a29eebefd1319c49d06cb1f00f
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-04 20:00:09 +0900

    feat: Make default theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+const size = {
+  mobile: "0px",
+  tablet: "769px",
+  desktop: "1023px",
+};
+
+const theme = {
+  deepBlue: "#093687",
+  mobile: `(min-width: ${size.mobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
+
+export default theme;
