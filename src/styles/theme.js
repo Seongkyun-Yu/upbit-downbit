e09d2e656bac6912c6d8c3cfefa3e6a788a8e561
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-05 16:19:20 +0900

    feat: Make transparent color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,7 +7,9 @@ const size = {
 const theme = {
   deepBlue: "#093687",
   priceDown: "#0051C7",
+  priceDownTrans: "rgba(0,81,199,0.3)",
   priceUp: "#D60000",
+  priceUpTrans: "rgba(214,0,0,0.3)",
   mobile: `(min-width: ${size.mobile})`,
   tablet: `(min-width: ${size.tablet})`,
   desktop: `(min-width: ${size.desktop})`,
