4e453af648de9d18ea35348fcb8985603487bf61
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-05 16:17:22 +0900

    feat: Add theme props

--- src/Components/withOHLCData.js
+++ src/Components/withOHLCData.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useSelector } from "react-redux";
+import { ThemeContext } from "styled-components";
 
 const withOHLCData = () => (OriginalComponent) => (props) => {
-  const selectedMarket = useSelector((state) => state.Coin.selectedMarket);
+  const selectedMarket = useSelector((state) => state.Coin.selectedMarket); // 선택된 코인/마켓
   const selectedCandles = useSelector(
     (state) => state.Coin.candle.data[selectedMarket].candles
-  );
-  const selectedTimeType = useSelector((state) => state.Coin.selectedTimeType);
+  ); // 선택된 코인/마켓 캔들 정보
+  const selectedTimeType = useSelector((state) => state.Coin.selectedTimeType); // 선택된 시간 타입
+  const theme = useContext(ThemeContext); // 테마 정보
 
   return !selectedCandles.length ? (
     <div className="center">Chart Loading</div>
@@ -15,6 +17,7 @@ const withOHLCData = () => (OriginalComponent) => (props) => {
       {...props}
       data={selectedCandles}
       selectedTimeType={selectedTimeType}
+      theme={theme}
     />
   );
 };
