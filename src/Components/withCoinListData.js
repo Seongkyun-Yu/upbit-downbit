97aaeb85ea85e059adcc23f572e35633d61a1e74
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-05 14:53:12 +0900

    feat: Add theme props

--- src/Components/withCoinListData.js
+++ src/Components/withCoinListData.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useSelector } from "react-redux";
+import { ThemeContext } from "styled-components";
 
 const withCoinListData = () => (OriginalComponent) => (props) => {
   const marketNames = useSelector((state) => state.Coin.marketNames.data);
   let marketNamesArr = Object.keys(marketNames);
   const coinListDatas = useSelector((state) => state.Coin.candle.data);
+  const theme = useContext(ThemeContext);
 
+  // 데이터 받는 데 성공하면 정렬한다
   if (Object.keys(coinListDatas).length > 1)
     marketNamesArr = marketNamesArr.sort((coin1, coin2) => {
       return (
@@ -14,12 +17,13 @@ const withCoinListData = () => (OriginalComponent) => (props) => {
       );
     });
 
-  return marketNamesArr ? (
+  return Object.keys(coinListDatas).length > 1 ? (
     <OriginalComponent
       {...props}
       marketNames={marketNames}
       marketNamesArr={marketNamesArr}
       coinListDatas={coinListDatas}
+      theme={theme}
     />
   ) : (
     <div>loading</div>
