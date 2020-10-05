36679854910eeb451a086a2b79d2b3d51d90b65a
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-05 14:26:44 +0900

    feat: Add priceUp & Down color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,6 +6,8 @@ const size = {
 
 const theme = {
   deepBlue: "#093687",
+  priceDown: "#0051C7",
+  priceUp: "#D60000",
   mobile: `(min-width: ${size.mobile})`,
   tablet: `(min-width: ${size.tablet})`,
   desktop: `(min-width: ${size.desktop})`,
