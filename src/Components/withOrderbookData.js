37f083b766a095810ce84244275edd21e8454d99
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-08 04:06:27 +0900

    fix: change buyOrderbook~~ to bidOrderbook~~ etc

--- src/Components/withOrderbookData.js
+++ src/Components/withOrderbookData.js
@@ -11,26 +11,30 @@ const withOrderbookData = () => (OriginalComponent) => (props) => {
     totalAskSize: orderbook.total_ask_size,
   };
 
-  const buyOrderbookData = [];
-  const sellOrderbookData = [];
+  const bidOrderbookData = [];
+  const askOrderbookData = [];
 
+  // 호가 데이터 분리 정렬
   orderbook.orderbook_units.forEach((orderbook) => {
-    sellOrderbookData.push({
-      askPrice: orderbook.ask_price,
-      askSize: orderbook.ask_size,
+    bidOrderbookData.push({
+      bidPrice: orderbook.bid_price,
+      bidSize: orderbook.bid_size,
     });
 
-    buyOrderbookData.push({
+    askOrderbookData.push({
       askPrice: orderbook.ask_price,
       askSize: orderbook.ask_size,
     });
   });
 
+  // 매도 호가창은 가격 내림차순으로 정렬해줌 (매수는 원래 가격 내림차순임)
+  askOrderbookData.sort((book1, book2) => +book2.askPrice - +book1.askPrice);
+
   return orderbook.orderbook_units.length ? (
     <OriginalComponent
       totalData={totalData}
-      buyOrderbookData={buyOrderbookData}
-      sellOrderbookData={sellOrderbookData}
+      bidOrderbookData={bidOrderbookData}
+      askOrderbookData={askOrderbookData}
     />
   ) : (
     <div>Orderbook Loading</div>
