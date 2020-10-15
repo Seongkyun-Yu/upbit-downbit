224c28756d05d49f4792166e650b93e4161973b0
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-15 20:38:39 +0900

    feat: Make name & logo

--- src/Components/CoinInfoHeader.js
+++ src/Components/CoinInfoHeader.js
@@ -1,10 +1,46 @@
+import { style } from "d3-selection";
 import React from "react";
 import styled from "styled-components";
 import withCoinInfoData from "../Container/withCoinInfoData";
 
 const CoinInfoContainer = styled.div`
-  width: 100%;
+  display: flex;
+  align-items: center;
+  width: 99%;
   background-color: white;
+  box-sizing: border-box;
+  padding: 10px;
+  margin-top: 10px;
+  border-bottom: 1px solid ${(props) => props.borderColor};
+  /* margin-right: 10px; */
+`;
+
+const CoinLogo = styled.i`
+  display: inline-block;
+  width: 35px;
+  height: 35px;
+  background-image: ${(props) =>
+    `url(https://static.upbit.com/logos/${props.coinNameEn}.png)`};
+  background-size: cover;
+  margin-left: 5px;
+`;
+
+const CoinNameContainer = styled.div`
+  padding: 0 8px 0 10px;
+`;
+
+const CoinName = styled.strong`
+  font-size: 1.78rem;
+  font-weight: 1500;
+  color: #2b2b2b;
+`;
+
+const CoinMarketName = styled.span`
+  display: flex;
+  font-size: 0.9rem;
+  flex-direction: column;
+  padding-left: 5px;
+  padding-top: 6px;
 `;
 
 const PriceInfo = styled.div`
@@ -20,6 +56,7 @@ const Price = styled.strong`
 const PriceUnit = styled.span`
   font-size: 0.9rem;
   font-weight: 500;
+  padding-left: 5px;
 `;
 
 const ChangeInfo = styled.span`
@@ -53,7 +90,12 @@ const CoinInfoHeader = ({
   priceColor,
 }) => {
   return (
-    <CoinInfoContainer>
+    <CoinInfoContainer borderColor={theme.lightGray2}>
+      <CoinLogo coinNameEn={coinNameEn} />
+      <CoinNameContainer>
+        <CoinName>{coinNameKor}</CoinName>
+        <CoinMarketName>{coinNameEn}/KRW</CoinMarketName>
+      </CoinNameContainer>
       <PriceInfo>
         <Price priceColor={priceColor}>
           {price}
