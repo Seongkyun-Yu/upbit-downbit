d78fc10abf5a8fc67be9bd55b3ac3859187114ba
Seongkyun-Yu <ysungkyun@gmail.com>
2020-11-05 23:27:08 +0900

    feat: Make withSelectedOption HOC

---
+++ src/Container/withSelectedOption.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const withSelectedOption = () => (OriginalComponent) => (props) => {
+  const coinState = useSelector((state) => state.Coin);
+
+  return (
+    <OriginalComponent
+      {...props}
+      selectedMarket={coinState.selectedMarket}
+      selectedTimeType={coinState.selectedTimeType}
+      selectedTimeCount={coinState.selectedTimeCount}
+      selectedAskBidOrder={coinState.selectedAskBidOrder}
+      searchCoin={coinState.searchCoin};
+    />
+  );
+};
