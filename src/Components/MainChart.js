cd48694bd98f55a6af3ba38d5021fcb8de76feb4
Seongkyun-Yu <ysungkyun@gmail.com>
2020-11-14 16:18:16 +0900

    fix: not displaying some coin data bug

--- src/Components/MainChart.js
+++ src/Components/MainChart.js
@@ -74,6 +74,7 @@ const MainChart = ({
   // // if (width > 1000) width = 1000;
   // width = width / 2;
   // console.log("메인차트 리랜더");
+  // console.log(initialData);
 
   const dateTimeFormat =
     selectedTimeType === "day" ? "%y-%m-%d" : "%y-%m-%d %H:%M";
@@ -117,8 +118,8 @@ const MainChart = ({
   );
 
   // 확대 축소 초기 범위를 정하는 xExtendts설정, max와 min이 변하면 새로운 데이터 추가시 줌 설정이 풀린다
-  const max = xAccessor(data[199]);
-  const min = xAccessor(data[100]);
+  const max = xAccessor(data[Math.min(199, data.length - 1)]);
+  const min = xAccessor(data[Math.min(50, Math.floor(data.length / 2))]);
   const xExtents = [min, max + 5];
 
   const gridHeight = height - margin.top - margin.bottom;
